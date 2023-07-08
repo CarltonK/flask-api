4926c020cb580e03e53fe2827907d8663b1637e3
Mark Carlton <mcarlton33@gmail.com>
2023-07-08 14:13:57 +0300

    flask starter

---
+++ starter.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('index.html', message = 'Aloha')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'Hawks' and password == 'password':
+            return render_template('login.html', message = username)
+        else:
+            error_message = 'Wrong password combination'
+            return render_template('error.html', message = error_message)
+
+@app.route('/work', methods = ['GET'])
+def work():
+    return render_template('work.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
